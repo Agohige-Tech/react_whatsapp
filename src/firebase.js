8475f9875b9a8478d5f09dde454303fb0c069b4c
Porter <crporter171@gmail.com>
2021-11-29 11:41:45 -0500

    Initial Clean Up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Ne9shcnAj3VEjx_xPpucKNzAxucx6sk",
+  authDomain: "agohige-messaging.firebaseapp.com",
+  projectId: "agohige-messaging",
+  storageBucket: "agohige-messaging.appspot.com",
+  messagingSenderId: "627100466185",
+  appId: "1:627100466185:web:aaf44f0b8a64b51c400cd8",
+  measurementId: "G-KK28GGR5JP",
+};
