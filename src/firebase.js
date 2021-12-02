78d8496320d7503830e7ca8e2d6f0a27fe94c54f
Porter <crporter171@gmail.com>
2021-12-01 20:33:19 -0500

    Starting the db procedures

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC3Ne9shcnAj3VEjx_xPpucKNzAxucx6sk",
   authDomain: "agohige-messaging.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
   appId: "1:627100466185:web:aaf44f0b8a64b51c400cd8",
   measurementId: "G-KK28GGR5JP",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
